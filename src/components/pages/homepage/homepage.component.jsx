5952ef7194ac2e7660608306692bccc3cfaaa0af
Andy Eshiet <eandy@groove.ng>
2020-02-10 19:40:46 +0100

    Updated Name

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.styles.scss";
+import NamePlaceholder from "../../placeholder/placeholder.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <NamePlaceholder></NamePlaceholder>
+  </div>
+);
+
+export default HomePage;
---
+++ src/components/placeholder/placeholder.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../placeholder/placeholder.styles.scss";
+
+const NamePlaceholder = () => (
+  <div className="name-placeholder">
+    <div className="content">
+      <h1 className="title"> Andy eshiet Inyang</h1>
+      <span className="subtitle"></span>
+    </div>
+  </div>
+);
+
+export default NamePlaceholder;
